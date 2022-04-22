b9a838570b3105521ee372afc21e70a2d8bd8276
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-23 01:19:03 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -96,21 +96,20 @@ function App() {
             Please note: You can not unstake; only withdraw earnings.
           </p>
           <ul>
-            <li>⭐Tokenomics:</li>
-            <li>🍀 Rewards can be re-invested & withdrawn daily.</li>
+            <li>Tokenomics:</li>
+            <li> Rewards can be re-invested & withdrawn daily.</li>
             <li>
-              ⭐To ensure the max rewards (10% we suggest the following
-              strategy:
+              To ensure the max rewards (10% we suggest the following strategy:
             </li>
-            <li>🍀 Plant Seeds (deposit bnb)</li>
-            <li>🍀 Feed weed 6 days per week </li>
+            <li> Plant Seeds (deposit bnb)</li>
+            <li> Feed weed 6 days per week </li>
             <li>
-              🍀 sell weed 1 day per week ( But the choice is yours you can
-              remove rewards daily )
+              sell weed 1 day per week ( But the choice is yours you can remove
+              rewards daily )
             </li>
             <li>
-              🍀 Every deposit/withdrawal has a base tax of 10% to ensure
-              contract longevity, along with an insurance fee
+              Every deposit/withdrawal has a base tax of 10% to ensure contract
+              longevity, along with an insurance fee
             </li>
           </ul>
         </div>
