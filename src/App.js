0cf5f10956e113c1ed8e6216aaa767d16ac78ea8
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-22 07:54:44 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./Assets/logo.svg";
+import nut from "./Assets/nut.svg";
+import social1 from "./Assets/social1.svg";
+import social2 from "./Assets/social2.svg";
+import social3 from "./Assets/social3.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="top-app">
+        <img src={logo} alt="" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The BNB Reward Pool with the tastiest daily return and lowest dev fee
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="bottom-app">
+        <div className="box">
+          <div className="row">
+            <p>Contract</p>
+            <p>7.92 BNB</p>
+          </div>
+          <div className="row">
+            <p>Wallet</p>
+            <p>0.00 BNB</p>
+          </div>
+          <div className="row">
+            <p>Your Nuts</p>
+            <p>0 NUTS</p>
+          </div>
+          <input type="text" className="input" />
+          <div className="btn-div">
+            <button className="connect">
+              connect Wallet <img src={nut} alt="" />
+            </button>
+            <button className="yellow-btn">Hoard Nuts</button>
+          </div>
+          <div className="row">
+            <p>Your Rewards</p>
+            <p>0.000 BNB</p>
+          </div>
+          <div className="btn-div">
+            <button className="compound">compound nuts</button>
+            <button className="yellow-btn">Hoard Nuts</button>
+          </div>
+        </div>
+        <div className="box">
+          <h2>Nutrition Facts</h2>
+          <div className="row">
+            <p>Daily Return</p>
+            <p>10%</p>
+          </div>
+          <div className="row">
+            <p>APR</p>
+            <p>3,650%</p>
+          </div>
+          <div className="row">
+            <p>Treasury Fee</p>
+            <p>10%</p>
+          </div>
+        </div>
+        <div className="box">
+          <h2>Referral Link</h2>
+          <input type="text" className="input" />
+          <p className="note">
+            Earn 10% of the BNB used to hoard beans from anyone who uses your
+            referral link
+          </p>
+        </div>
+        <div className="social-div">
+          <a href="#">
+            <img src={social1} alt="" />
+          </a>
+          <a href="#">
+            <img src={social2} alt="" />
+          </a>
+          <a href="#">
+            <img src={social3} alt="" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
